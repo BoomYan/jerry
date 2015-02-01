cc5f5ea932f1ef444283ec38319f39a3bd684ab9
BoomYan <jiamingyan@hotmail.com>
2015-02-01 11:28:14 -0500

    try nodejs express and basic threejs

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var ejs = require('ejs');
+
+app.set('views', __dirname); //????????????
+
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(1234);
+
+//app.get('/', );
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+//event listener 
+io.on('connection', function (socket) {
+
+	var obj = {
+		'hello': 'world'
+	};
+  socket.emit('news', obj);
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('wtf', function(data){
+  	console.log(socket.id);
+  	socket.broadcast.emit('wtf', data);
+  });
+
+});
